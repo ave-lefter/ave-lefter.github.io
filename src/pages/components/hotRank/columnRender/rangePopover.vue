a8509e22d5d7a3236f58306a62e6cc2a4f30a734
missop <html.css.tscript@gmail.com>
2025-07-10 13:26:49 +0800

    feat: hotRank add

---
+++ src/pages/components/hotRank/columnRender/rangePopover.vue
@@ -0,0 +1,111 @@
+<script setup lang="ts">
+const props = defineProps<{
+  modelValue: boolean
+  width: number
+  title: string
+  list: { text: string; value: string }[]
+  selectRangeIndex: 0 | 1
+  isFilterHighlight: boolean
+}>()
+
+const emit = defineEmits(['update:modelValue', 'confirm'])
+const popoverVisible = computed({
+  get() {
+    return props.modelValue
+  },
+  set(val) {
+    emit('update:modelValue', val)
+  },
+})
+
+const rangeArr = ref<[string, string]>(['', ''])
+function confirm(params?: [string, string]) {
+  if (!params) {
+    rangeArr.value = ['', '']
+  }
+  emit('confirm', params)
+  popoverVisible.value = false
+}
+const themeStore = useThemeStore()
+function onSelect(item:{
+    text: string;
+    value: string;
+}) {
+    if(!item.value){
+        rangeArr.value[0]=''
+        rangeArr.value[1]=''
+    } else {
+        rangeArr.value[props.selectRangeIndex] = item.value
+    }
+}
+</script>
+
+<template>
+  <el-popover
+    v-model:visible="popoverVisible"
+    placement="bottom"
+    title=""
+    :width="width"
+    trigger="click"
+  >
+    <template #reference>
+      <Icon
+        name="custom:filter"
+        class="text-10px cursor-pointer"
+        :class="isFilterHighlight ? 'color-[--d-999-l-666]' : ''"
+      />
+    </template>
+    <template #default>
+      <div class="text-12px font-400">
+        {{ title }}
+      </div>
+      <ul class="mt-10px">
+        <li v-for="(item, index) in list" :key="index">
+          <a
+            href="javascript:;"
+            class="flex items-center justify-center text-12px leading-16px font-400 border border-solid border-[--d-333-l-F5F5F5] px-15px py-8px text-center mb-10px rounded-4px"
+            :class="
+              item.value === rangeArr[selectRangeIndex]
+                ? 'border-[--d-F5F5F5-l-333] bg-[--d-333-l-F5F5F5]'
+                : ''
+            "
+            @click.stop.prevent="onSelect(item)"
+          >
+            {{ item.text }}
+          </a>
+        </li>
+      </ul>
+      <div class="flex items-center mt-10px">
+        <el-input
+          v-model.trim.number="rangeArr[0]"
+          clearable
+          type="text"
+          @input="(value) => (rangeArr[0] = value.replace(/\-|[^\d.]/g, ''))"
+        >
+          <template #append>h</template>
+        </el-input>
+        <span class="ml-10px mr-10px">~</span>
+        <el-input
+          v-model.trim.number="rangeArr[1]"
+          clearable
+          type="text"
+          @input="(value) => (rangeArr[1] = value.replace(/\-|[^\d.]/g, ''))"
+        >
+          <template #append>h</template>
+        </el-input>
+      </div>
+      <div class="mt-20px flex">
+        <el-button
+          class="h-30px flex-1 m-l-auto"
+          :color="themeStore.isDark ? '#333' : '#F2F2F2'"
+          @click="confirm()"
+        >
+          {{ $t('reset') }}
+        </el-button>
+        <el-button type="primary" class="h-30px flex-1 m-l-auto" @click="confirm(rangeArr)">
+          {{ $t('confirm') }}
+        </el-button>
+      </div>
+    </template>
+  </el-popover>
+</template>
