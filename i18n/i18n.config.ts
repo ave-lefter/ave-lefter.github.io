9ddd36b4f1e00b9333f668851f39b293848704bb
missop <html.css.tscript@gmail.com>
2025-07-22 16:47:19 +0800

    ix: bug

--- i18n/i18n.config.ts
+++ i18n/i18n.config.ts
@@ -1,13 +1,13 @@
-const waring = new Set()
+// const waring = new Set()
 
 export default {
   legacy: false,
   fallbackLocale: 'en',
   // messages 是可选的，因为启用了 lazy
-  missing(locale,key,vm,values){
-    waring.add(key)
-    setTimeout(()=>{
-      console.log(waring,"warning",[...waring].join(" "))
-    },1000)
-  }
+  // missing(locale,key,vm,values){
+  //   waring.add(key)
+  //   setTimeout(()=>{
+  //     console.log(waring,"warning",[...waring].join(" "))
+  //   },1000)
+  // }
 }
