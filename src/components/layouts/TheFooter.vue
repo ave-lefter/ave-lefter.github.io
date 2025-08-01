87641b7325f04f197de65b67a10ad73badf33805
lewis <691068928@qq.com>
2025-08-01 20:50:51 +0800

    fix:

--- src/components/layouts/TheFooter.vue
+++ src/components/layouts/TheFooter.vue
@@ -81,7 +81,7 @@
       <li class="color-[--d-999-l-666] hover:color-[--d-FFF-l-000] flex items-center gap-2px">
       <a target="_blank" href="https://cloud.ave.ai">API</a>
       </li>
-      <el-popover popper-style="padding: 12px;min-width: 107px" width="107" placement="top" :teleported="false">
+      <el-popover popper-style="padding: 12px;min-width: 50px;width:auto"  placement="top" :teleported="false">
       <template #reference>
           <Icon name="custom:set-up" class="text-12px ml-2px color-#666" />
       </template>
