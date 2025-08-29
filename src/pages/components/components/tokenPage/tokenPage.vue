f7fdc110dd32c358e0d5ceba3a7271ba3b2b4efe
Lakes <sakirajessica01@gmail.com>
2025-08-29 10:36:00 +0800

    fix: fix rank kline data

--- src/pages/components/components/tokenPage/tokenPage.vue
+++ src/pages/components/components/tokenPage/tokenPage.vue
@@ -8,7 +8,7 @@ import TokenTop from './tokenTop.vue'
   <div class="sticky left-0 flex w-100vw box-border h-360px border-1px border-solid border-[--d-566275-l-8CA0C3]">
     <div class="flex-1">
         <TokenTop/>
-        <Kline isRank/>
+        <Kline :key="useRankKlineStore().pairAddress" isRank/>
     </div>
     <TokenInfo/>
   </div>
