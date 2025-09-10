8915049352fb7b8101939cb568167a8440e0a5b7
missop <html.css.tscript@gmail.com>
2025-09-10 15:10:42 +0800

    feat: dex filter

--- src/pages/components/components/dexHeader.vue
+++ src/pages/components/components/dexHeader.vue
@@ -3,6 +3,7 @@ import type { IGetTreasureConfig } from '~/api/market'
 
 const props = defineProps<{
     ammList: IGetTreasureConfig['swaps']
+    setFilterForm(...args: [string, string][]): void
 }>()
 
 const {t} = useI18n()
@@ -16,8 +17,15 @@ const ammOptions = computed(()=>{
         }
     ].concat(props.ammList)
 })
-const activeAmm = ref('')
+const globalStore = useGlobalStore()
+const setupFilter = globalStore.rankConditions[globalStore.rankActiveTab]?.filter
+const isFilterHighlight = ref(!!setupFilter.amm)
 const searchKey = ref('')
+const filteredAmmList = computed(()=>{
+    return ammOptions.value.filter((el)=>{
+        return el.name.includes(searchKey.value)
+    })
+})
 </script>
 
 <template>
@@ -38,7 +46,16 @@ const searchKey = ref('')
                 <Icon name="hugeicons:search-01" />
                 </template>
             </el-input>
-            <el-scrollbar :height="300"/>
+            <el-scrollbar v-if="filteredAmmList.length > 0" :height="300">
+                <ul>
+                    <li v-for="item in filteredAmmList" :key="item.name">
+                        <div class="flex items-center gap-2px">
+                            <div class="w-10px h-10px rounded-full" :style="{background:item.chain}"/>
+                            <span>{{ item.show_name }}</span>
+                        </div>
+                    </li>
+                </ul>
+                </el-scrollbar>
         </template>
     </el-popover>
   </div>
