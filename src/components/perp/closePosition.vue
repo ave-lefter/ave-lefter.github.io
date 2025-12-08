8ea31a7a0484ce0bc8df85bbc230d60a3d82dc70
Lakes <sakirajessica01@gmail.com>
2025-12-08 21:28:03 +0800

    fix: fix close position bug

--- src/components/perp/closePosition.vue
+++ src/components/perp/closePosition.vue
@@ -163,8 +163,8 @@ function _createOrder() {
       <span :class="BigNumber(unrealizedPnl).gte(0) ? 'color-[--up-color]' : 'color-[--down-color]'">{{ unrealizedPnl }} USD</span>
     </div>
     <div class="flex mt-20px text-16px">
-      <el-button class="h-30px flex-1 m-l-auto" :color="themeStore.isDark ? '#333' : '#F2F2F2'">
-        {{ $t('reset') }}
+      <el-button class="h-30px flex-1 m-l-auto" :color="themeStore.isDark ? '#333' : '#F2F2F2'" @click.stop="visible=false">
+        {{ $t('cancel') }}
       </el-button>
       <el-button type="primary" class="h-30px flex-1 m-l-auto" :disabled="disabled" :loading="loading" @click.stop="_createOrder">
         {{ $t('confirm') }}
