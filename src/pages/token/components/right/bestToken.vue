7cd734d29523e68be91b3037c29d73072c40ac42
june <june@ave.com>
2026-01-14 15:42:42 +0800

    feat: enhance bestToken component with click functionality and code formatting

--- src/pages/token/components/right/bestToken.vue
+++ src/pages/token/components/right/bestToken.vue
@@ -4,7 +4,7 @@
     class="flex items-center py-11px px-8px text-12px justify-between bg-[--dialog-bg] rounded-4px mb-12px"
   >
     {{ $t('devBestToken') }}
-    <div class="flex items-center color-[--third-text]">
+    <div class="flex items-center color-[--third-text] cursor-pointer" @click="tokenClick">
       <TokenImg
         class="mr-4px"
         :row="{
@@ -13,7 +13,11 @@
           chain: chain || '',
         }"
       />
-      <span v-tooltip="bestToken.symbol" class="font-500 text-16px color-[--main-text] mr-2px max-w-60px truncate">{{ bestToken.symbol }}</span>
+      <span
+        v-tooltip="bestToken.symbol"
+        class="font-500 text-16px color-[--main-text] mr-2px max-w-60px truncate"
+        >{{ bestToken.symbol }}</span
+      >
       (ATH MC
       <span class="color-[--main-text] ml-2px mr-2px"
         >${{ formatNumber(bestToken.all_time_high, 0) }}</span
@@ -27,10 +31,15 @@ import { getBestToken } from '~/api/token'
 const route = useRoute()
 const bestToken = ref(null)
 const chain = computed(() => getAddressAndChainFromId(route.params.id || '').chain)
+
+const tokenClick = () => {
+  const { token } = bestToken.value
+  navigateTo(`/token/${token}-${chain.value}`)
+}
+
 const _getBestToken = async () => {
   try {
     const res = await getBestToken(route.params.id || '')
-    console.log(res,'res')
     bestToken.value = res?.symbol ? res : res.data
   } catch (error) {
     console.error('Error fetching best token:', error)
@@ -39,7 +48,10 @@ const _getBestToken = async () => {
 
 _getBestToken()
 
-watch(()=>route.params.id,()=>{
-  _getBestToken()
-})
+watch(
+  () => route.params.id,
+  () => {
+    _getBestToken()
+  }
+)
 </script>
