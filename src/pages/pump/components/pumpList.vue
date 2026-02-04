361722905d77bbfc4e0f4258076cb470f495cbd3
amy8023 <2847361548@qq.com>
2026-02-04 16:39:49 +0800

    fix: fix bug

--- src/pages/pump/components/pumpList.vue
+++ src/pages/pump/components/pumpList.vue
@@ -392,32 +392,24 @@
                       <span v-else class="color-[--main-text]">{{ formatNumber(row?.smart_wallet_tag_count || 0, 2) }}</span>
                     </div>
                   </div>
-                  <div class="mt-5px flex-start">
-                    <PumpPop
-                        v-if="row?.medias?.some(i=> i.icon === 'twitter') && route.name === 'index'"
-                        :tokenId="(row.token + '-' + row.chain)"
-                        :type="2"
-                      >
-                    <a v-for="(item, index) in row?.medias?.filter(i => i.icon === 'twitter')" :key="index"
-                      :href="item.url" target="_blank" class="!color-#009EF7 mr-5px" @click.stop>
-                          {{ formatXUser(item?.url) }}
-                        </a>
-                    </PumpPop>
+                  <div class="mt-5px flex-start text-11px">
                     <template
                       v-if="row.buy_tax && row.sell_tax"
                     >
                       <span
                         v-if="row.buy_tax == row.sell_tax"
+                        class="mr-5px"
                         :style="{
-                              color:(Number(row?.sell_tax) > 5 ? '#F6465D' : 'var(--third-text)'),
+                              color:(Number(row?.sell_tax) > 5 ? '#F6465D' : 'var(--secondary-text)'),
                           }"
                       >
                         Tax {{ formatNumber(row?.sell_tax || 0, 2) }}%
                       </span>
                       <span
                         v-else
+                        class="mr-5px"
                           :style="{
-                              color: (Number(row?.sell_tax) > 5 || Number(row?.buy_tax) > 5  ? '#F6465D' :'var(--third-text)'),
+                              color: (Number(row?.sell_tax) > 5 || Number(row?.buy_tax) > 5  ? '#F6465D' :'var(--secondary-text)'),
                           }"
                       >
                       B {{ formatNumber(row?.buy_tax || 0, 2) }}%&nbsp;&nbsp;S {{ formatNumber(row?.sell_tax || 0, 2) }}%
@@ -425,20 +417,32 @@
                     </template>
                     <span
                       v-else-if="row.buy_tax"
+                      class="mr-5px"
                       :style="{
-                            color:(Number(row?.buy_tax) > 5 ? '#F6465D' : 'var(--third-text)'),
+                            color:(Number(row?.buy_tax) > 5 ? '#F6465D' : 'var(--secondary-text)'),
                         }"
                     >
                       B {{ formatNumber(row?.buy_tax || 0, 2) }}%
                     </span>
                     <span
                       v-else-if="row.sell_tax"
+                      class="mr-5px"
                         :style="{
-                            color:(Number(row?.sell_tax) > 5 ? '#F6465D' : 'var(--third-text)'),
+                            color:(Number(row?.sell_tax) > 5 ? '#F6465D' : 'var(--secondary-text)'),
                         }"
                     >
                       S {{ formatNumber(row?.sell_tax || 0, 2) }}%
                     </span>
+                    <PumpPop
+                        v-if="row?.medias?.some(i=> i.icon === 'twitter') && route.name === 'index'"
+                        :tokenId="(row.token + '-' + row.chain)"
+                        :type="2"
+                      >
+                    <a v-for="(item, index) in row?.medias?.filter(i => i.icon === 'twitter')" :key="index"
+                      :href="item.url" target="_blank" class="!color-#009EF7" @click.stop>
+                          {{ formatXUser(item?.url) }}
+                        </a>
+                    </PumpPop>
                   </div>
                   <div class="flex-start text-12px absolute bottom--2px z-1 mt-5px">
                     <div
