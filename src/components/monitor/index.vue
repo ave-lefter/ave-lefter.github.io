c1aae9d1ca0f8d0c2ebb724b871487602e510ae4
lewis <691068928@qq.com>
2026-02-06 19:03:08 +0800

    fix:

--- src/components/monitor/index.vue
+++ src/components/monitor/index.vue
@@ -121,7 +121,7 @@ class="w-monitor bg-[--secondary-bg] w-100% h-100% pl-12px pr-6px relative overf
                 <TokenImg
                 :row="{
                   logo_url: row?._target_Token?.logo_url,
-                  chain: row?.chain
+                  chain: ''
                 }" token-class="w-16px h-16px [&&]:mr-4px" />
                   <span>{{ row?._target_Token?.symbol }}</span>
                   <img v-if="row?.amm=='pump'"  src="https://www.iconaves.com/signals/pump_king.png" style="width:12px;height:12px">
@@ -233,7 +233,7 @@ class="w-monitor bg-[--secondary-bg] w-100% h-100% pl-12px pr-6px relative overf
                       <TokenImg
                         :row="{
                           logo_url: row?._target_Token?.logo_url,
-                          chain: row?.chain
+                          chain: ''
                         }" token-class="w-16px h-16px [&&]:mr-4px" />
                           <span class="color-[--main-text]">{{ row?._target_Token?.symbol }}</span>
                           <img v-if="row?.amm=='pump'"  src="https://www.iconaves.com/signals/pump_king.png" style="width:12px;height:12px">
