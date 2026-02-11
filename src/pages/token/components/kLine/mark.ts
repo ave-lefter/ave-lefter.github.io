ccda4a66977c0a4d5d9e9274555a6321a7498fd7
j2 <xml@inc.com>
2026-02-11 16:52:41 +0800

    fix: bug

--- src/pages/token/components/kLine/mark.ts
+++ src/pages/token/components/kLine/mark.ts
@@ -4,7 +4,6 @@ import { useLocalStorage, type RemovableRef } from '@vueuse/core'
 import type { IChartingLibraryWidget, Mark } from '~/types/tradingview/charting_library'
 import { getUserKlineTxTags, getKlineProfilingTagsV2, type IGetKlineProfilingTagsV2Item,  type HolderBuy, type WalletLogo } from '@/api/token'
 import type { SimpleWSTx, WSTx } from './types'
-import { isJSDocNonNullableType } from 'typescript'
 
 type TradeSide = {
   amount: number
