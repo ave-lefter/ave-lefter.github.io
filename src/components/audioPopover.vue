5a0870eba5767bc7cc1d0bebb3e65376feba9676
lewis <691068928@qq.com>
2026-02-13 10:53:01 +0800

    fix: audioPop placement

--- src/components/audioPopover.vue
+++ src/components/audioPopover.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-popover placement="top-start" popper-class="el-select__popper" popper-style="width: 150px;min-width: 150px; padding: 6px 0" trigger="click" ref="audioPopoverRef"  virtual-triggering :virtual-ref="buttonRef"
+  <el-popover placement="right-start" popper-class="el-select__popper" popper-style="width: 150px;min-width: 150px; padding: 6px 0" trigger="click" ref="audioPopoverRef"  virtual-triggering :virtual-ref="buttonRef"
 >
       <ul  class="el-select-dropdown__list group">
         <li v-for="item in audioList" :key="item" class="el-select-dropdown__item text-[--main-text] flex-between" :class="audioSettings.audio?.[type]===item?'text-[--primary-color]!':''" @click="()=>handleAudioSelect(item)">
