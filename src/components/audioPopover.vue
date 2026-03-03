89f4ac09c0ddb10618ac9f249ff0db11b973233c
luweijun <luweijun@jfgenius.com>
2026-03-03 11:27:17 +0800

    fix：previewAudio

--- src/components/audioPopover.vue
+++ src/components/audioPopover.vue
@@ -2,7 +2,7 @@
   <el-popover placement="right-start" popper-class="el-select__popper" popper-style="width: 150px;min-width: 150px; padding: 6px 0" trigger="click" ref="audioPopoverRef"  virtual-triggering :virtual-ref="buttonRef"
 >
       <ul  class="el-select-dropdown__list group">
-        <li v-for="item in audioList" :key="item" class="el-select-dropdown__item text-[--main-text] flex-between" :class="audioSettings.audio?.[type]===item?'text-[--primary-color]!':''" @click="()=>handleAudioSelect(item)">
+        <li v-for="item in audioList" :key="item" class="el-select-dropdown__item text-[--main-text] flex-between" :class="audioSettings.audio?.[type]===item?'text-[--primary-color]!':''" @click="()=>handleAudioSelect(item)" @mouseenter="()=>handleMouseEnter(item)" @mouseleave="handleMouseLeave">
           <span>{{ item ? item : $t('close') }}</span>
           <Icon v-if="audioSettings.audio?.[type]===item" name="material-symbols:check"  class="text-16px color-[--main-text]"/>
         </li>
@@ -20,11 +20,46 @@ const props = defineProps<{
 
 const { audioSettings } = storeToRefs(useGlobalStore())
 const audioPopoverRef = ref<PopoverInstance>()
- 
-function handleAudioSelect(item:string){ 
+const hoverTimer = ref<ReturnType<typeof setTimeout> | null>(null)
+const previewAudio = ref<HTMLAudioElement | null>(null)
+
+function stopPreview() {
+  if (hoverTimer.value) {
+    clearTimeout(hoverTimer.value)
+    hoverTimer.value = null
+  }
+  if (previewAudio.value) {
+    previewAudio.value.pause()
+    previewAudio.value.currentTime = 0
+    previewAudio.value = null
+  }
+}
+
+function handleMouseEnter(item: string) {
+  stopPreview()
+  if (!item) return
+  hoverTimer.value = setTimeout(() => {
+    const src = audioNameToResource[item as keyof typeof audioNameToResource]
+    if (src) {
+      previewAudio.value = new Audio(src)
+      previewAudio.value.play()
+    }
+  }, 500)
+}
+
+function handleMouseLeave() {
+  stopPreview()
+}
+
+function handleAudioSelect(item:string){
+  stopPreview()
   audioPopoverRef.value?.hide()
   audioSettings.value.audio[props.type]=item
 }
+
+onUnmounted(() => {
+  stopPreview()
+})
 </script>
 
 <style scoped>
