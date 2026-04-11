40f2eeaf821f1c09f84937f4333f00d41e6ceb34
lewis <691068928@qq.com>
2026-04-11 20:51:21 +0800

    fix: unused scrollHeight from tw

--- src/components/twitterTracker/index.vue
+++ src/components/twitterTracker/index.vue
@@ -58,12 +58,15 @@ const {
 })
 
 // Override childProps for twitter tracker's specific needs
-const trackerChildProps = computed(() => ({
-  ...childProps.value,
-  class: placement.value === 'center'
-    ? 'border-1px border-solid border-[--d-1A1A1A-l-F2F2F2] shadow-[0_5px_10px_0_var(--d-FFFFFF14-l-00000014)]'
-    : undefined
-}))
+const trackerChildProps = computed(() => {
+  const { scrollHeight, ...restProps } = childProps.value
+  return {
+    ...restProps,
+    class: placement.value === 'center'
+      ? 'border-1px border-solid border-[--d-1A1A1A-l-F2F2F2] shadow-[0_5px_10px_0_var(--d-FFFFFF14-l-00000014)]'
+      : undefined
+  }
+})
 
 function subscribePublicTwitter(method) {
   v2WsStore.send({
